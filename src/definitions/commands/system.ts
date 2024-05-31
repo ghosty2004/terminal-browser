7c54db12eac6acd35e92c7f66305dc3f54b7d217
ghosty2004 <fejerszilveszter@gmail.com>
2024-05-31 18:30:16 +0300

    feat: add more commands for system

--- src/definitions/commands/system.ts
+++ src/definitions/commands/system.ts
@@ -1,5 +1,18 @@
 import CommandBuilder from "../../utils/CommandBuilder";
+import { DEFAULT_CWD, DEFAULT_HOSTNAME, DEFAULT_USERNAME } from "../defaults";
 
 new CommandBuilder("echo").handle(({ print, args }) => {
     print(args.join(" ").replace(/["']/g, ""));
+});
+
+new CommandBuilder("whoami").handle(({ print }) => {
+    print(DEFAULT_USERNAME);
+});
+
+new CommandBuilder("pwd").handle(({ print }) => {
+    print(DEFAULT_CWD);
+});
+
+new CommandBuilder("hostname").handle(({ print }) => {
+    print(DEFAULT_HOSTNAME);
 });
